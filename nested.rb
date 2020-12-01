e15cadf0b7025c758bff007407bc6195eea69b0b
Nathan K Gusky <n.gusky88@gmail.com>
2020-12-01 00:00:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,8 +134,7 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C", "Assembly"]
       }
     }
-programmer_hash[:dennis_ritchies_language]<<"Assembly"
 end
